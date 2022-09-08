b646b942a4785eee7ab008b7955641ee3e31d3af
aranramesh <svramesh692@gmail.com>
2022-09-08 10:06:59 +0530

    js objects

--- css/index.js
+++ css/index.js
@@ -49,3 +49,56 @@ addInput.addEventListener('keyup', (event) => {
     addLists();
   }
 });
+
+// second topic 
+
+
+	
+function calculate(){
+	var unitprice,qty,total,
+	
+	 documents=this.parentNode.parentNode,
+	 unitprices=documents.getElementsByClassName("unitprice")[0],
+	 qtys=documents.getElementsByClassName("qty")[0],
+	 totals=documents.getElementsByClassName("total")[0];
+	
+	unitprice=unitprices.innerText;
+	qty=qtys.value;
+	total=unitprice*qty;
+	totals.innerText=total;
+	}
+	
+	
+	let btn=document.getElementsByClassName("btn");
+	
+	
+	for(var i=0;i<btn.length; i++){
+	
+		btn[i].addEventListener("click",calculate);
+	}
+	
+	
+	function totalprice(){
+	
+	var unitprice,qty,total,
+	
+	documents=this.parentNode.parentNode,
+	unit=documents.getElementsByClassName("unitprice")[0],
+	quantity=documents.getElementsByClassName("qty")[0],
+	overallprice=documents.getElementsByClassName("total")[0];
+	
+	
+	unitprice=unit.innerText;
+	qty=quantity.value;
+	total=unitprice*qty;
+	overallprice.innerText=total;
+	
+	}
+	
+	let btns=document.getElementsByClassName("btn")
+	
+	for(i=0; i<btns.length; i++){
+		btns[i].addEventListener("click",totalprice);
+	}
+
+
